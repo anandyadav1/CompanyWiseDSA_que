f8309f4172766fe558ad871535c852b9e1499a35
anandyadav1 <ay4799958@gmail.com>
2025-04-02 23:45:54 +0530

    two integers k and x return the k closest integer to x in the array

---
+++ Amazon/medium/kClosestElement.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.PriorityQueue;
+
+public class kClosestElement {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7};
+        int k=4;
+        int x=9;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0; i<k; i++){
+            pq.add(arr[i]);
+        }
+        for(int i=k; i<arr.length; i++){
+            if(Math.abs(x-pq.peek()) > Math.abs(x-arr[i])){
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+        while (!pq.isEmpty()) {
+            System.out.print(pq.poll()+" ");
+        }
+    }
+}
