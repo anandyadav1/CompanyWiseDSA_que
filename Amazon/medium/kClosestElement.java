df31417db07bc895d0aecd102103ed7d54cea32c
anandyadav1 <ay4799958@gmail.com>
2025-04-02 23:51:06 +0530

    k closest Element

--- Amazon/medium/kClosestElement.java
+++ Amazon/medium/kClosestElement.java
@@ -1,11 +1,10 @@
-import java.util.*;
 import java.util.PriorityQueue;
 
 public class kClosestElement {
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5,6,7};
         int k=4;
-        int x=9;
+        int x=3;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i=0; i<k; i++){
             pq.add(arr[i]);
