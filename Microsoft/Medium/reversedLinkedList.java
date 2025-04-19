7b2ba8885dc30097655126ce00a921d1f6415efe
anandyadav1 <ay4799958@gmail.com>
2025-04-19 14:11:44 +0530

    A singly LinkedList is made to the reversed LinkedList

--- Microsoft/Medium/reversedLinkedList.java
+++ Microsoft/Medium/reversedLinkedList.java
@@ -1,49 +1,59 @@
-class Node{
+class Node {
     int val;
     Node next;
-    Node(int v){
-        val=v;
+
+    Node(int v) {
+        val = v;
     }
 }
+
 public class reversedLinkedList {
-    public static Node reverseFunctions(Node head){
-        Node prev=null,curr=head, next;
+    public static Node reverseFunctions(Node head) {
+        Node prev = null, curr = head, next;
         while (curr != null) {
-            next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
 
         return prev;
     }
-    public static void main(String[] args) {
-        int nums[]={1,2,3,4,5,6,7,8,9};
-        Node head =new Node(nums[0]);
-        Node temp=head;
-        for(int i=1; i<nums.length; i++){
-            temp.next=new Node(nums[i]);
-            temp=temp.next;
-        }
-        temp.next=null;
 
-        Node tempNode=head;
-        System.out.println("Before LinkedList ");
-        while (tempNode!=null) {
-            System.out.print(tempNode.val);
-            if(tempNode.next!=null){
-                System.out.print("->");
-            }
-            tempNode=tempNode.next;
+    public static void main(String[] args) {
+        int nums[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        /*Output
+         * Before LinkedList
+         * 1->2->3->4->5->6->7->8->9
+         * After Revrsed LinkedList
+         * 9->8->7->6->5->4->3->2->1
+         */
+
+        Node head = new Node(nums[0]);
+        Node temp = head;
+        for (int i = 1; i < nums.length; i++) {
+            temp.next = new Node(nums[i]);
+            temp = temp.next;
         }
-        Node revNode=reverseFunctions(head);
-        System.out.println("\nAfter Revrsed LinkedList ");
-        while (revNode!=null) {
-            System.out.print(revNode.val);
-            if(revNode.next!=null){
+        temp.next = null;
+
+        Node tempNode = head;
+        System.out.println("Before LinkedList ");
+        while (tempNode != null) {
+            System.out.print(tempNode.val);
+            if (tempNode.next != null) {
                 System.out.print("->");
             }
-            revNode=revNode.next;
+            tempNode = tempNode.next;
+        }
+        Node revNode = reverseFunctions(head);
+        System.out.println("\nAfter Revrsed LinkedList ");
+        while (revNode != null) {
+            System.out.print(revNode.val);
+            if (revNode.next != null) {
+                System.out.print("->");
+            }
+            revNode = revNode.next;
         }
     }
 }
