a8f3ce5eddc434b65527d353ce73cddb1c8cafda
anandyadav1 <ay4799958@gmail.com>
2025-04-19 09:45:31 +0530

    Defanging an IP Address

---
+++ Microsoft/Easy/ipAddress.java
@@ -0,0 +1,23 @@
+public class ipAddress {
+    public static String convert(String address){
+        StringBuilder sb = new StringBuilder();
+        for(char ch : address.toCharArray()){
+            if(ch=='.'){
+                sb.append("[.]");
+            }else{
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+
+        //Or
+
+        // return address.replace(".", "[.]");
+    }
+    public static void main(String[] args) {
+        String address="1.1.1.1.1.1";
+        //output -> 1[.]1[.]1[.]1
+
+        System.out.println(convert(address));
+    }
+}
